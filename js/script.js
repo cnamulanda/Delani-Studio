e7fc33766c72fdd6054cfb2bdb169a90a8e81621
Christine <christine.namulanda@gmail.com>
2019-06-11 08:39:32 -0400

    update made

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
-    $(".icon-hidden").click(function() {
-      $(".icon-showing").toggle();
-      $(".icon-hidden").toggle();
+    $(".icon-hiddens ").click(function() {
+      $(".icon-showings ").show();
+      $(".icon-hiddens ").hide();
     });
-    $(".icon-showing").click(function() {
-        $(".icon-hidden").toggle();
-        $(".icon-showing").toggle();
+    $(".icon-showings ").click(function() {
+        $(".icon-hiddens ").show();
+        $(".icon-showings ").hide();
       });
+      $(".icon-hid").click(function() {
+        $(".icon-show ").show();
+        $(".icon-hid").hide();
+      });
+      $(".icon-show ").click(function() {
+          $(".icon-hid").show();
+          $(".icon-show ").hide();
+     });
+     $(".icon-hidden ").click(function() {
+        $(".icon-showing ").show();
+        $(".icon-hidden ").hide();
+      });
+      $(".icon-showing ").click(function() {
+          $(".icon-hidden ").show();
+          $(".icon-showing ").hide();
+        });
+     
+      
 
       
     
